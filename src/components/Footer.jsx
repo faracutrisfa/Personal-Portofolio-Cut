69d6bbde920a86a28b68a3454a781716bf802a6d
faracutrisfa <faracutrisfa@gmail.com>
2024-06-07 20:31:23 +0700

    style: adding style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
       <div className="bg-primary-50 flex flex-col items-center text-sm">
-        <p>© 2023 | Designed ❤️️ by Sagar Shah </p>
-        <p>© 2024 | Code ❤️️ by Cut Risfa Zuhra </p>
+        <a href="https://www.figma.com/@shahsagarm" target="_blank">© 2023 | Designed ❤️️ by Sagar Shah</a>
+        <a href="https://github.com/faracutrisfa/Personal-Portofolio-Cut.git" target="_blank">© 2024 | Code ❤️️ by Cut Risfa Zuhra</a>
       </div>
     )
   }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
+import Logo from "../assets/image/logoCut.svg"
+
 export default function Navbar() {
     return (
-      <div>This is Navbar</div>
+      <nav>
+        <img src={Logo} alt="logo" />
+        <ul>
+          <li>About</li>
+          <li>Work</li>
+          <li>Testimonials</li>
+          <li>Contact</li>
+        </ul>
+        <a href="https://drive.google.com/drive/folders/1sP9alvtJpKOGglPtPbCgJfGnJly-9Ogf?usp=sharing" target="blank">Download CV</a>
+      </nav>
     )
   }
