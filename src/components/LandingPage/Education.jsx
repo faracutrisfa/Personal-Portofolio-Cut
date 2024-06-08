7267839c416027e337eb6a250a2f7f57118857fb
faracutrisfa <faracutrisfa@gmail.com>
2024-06-09 01:06:44 +0700

    style: add style contact

--- src/components/LandingPage/Education.jsx
+++ src/components/LandingPage/Education.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export default function Education() {
   return (
-    <section id="Education" className="lg:px-20 px-4 py-24 h-auto">
+    <section id="Education" className="lg:px-20 px-4 py-24 h-auto bg-primary-50">
       <div className="flex flex-col items-center gap-4">
         <motion.button
           whileTap={{
--- src/components/LandingPage/Experience.jsx
+++ src/components/LandingPage/Experience.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export default function Experience() {
   return (
-    <section id="Experience" className="lg:px-20 px-4 py-24 h-auto">
+    <section id="Experience" className="lg:px-20 px-4 py-24 h-auto bg-primary-50">
       <div className="flex flex-col items-center gap-4">
         <motion.button
           whileTap={{
