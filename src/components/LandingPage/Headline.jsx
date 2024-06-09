efe65f4c7e07cce575b59d0a04ba6901647ef912
faracutrisfa <faracutrisfa@gmail.com>
2024-06-09 14:52:38 +0700

    style: add style headline

--- src/components/LandingPage/Headline.jsx
+++ src/components/LandingPage/Headline.jsx
@@ -6,27 +6,27 @@ import { CiLinkedin } from "react-icons/ci";
 
 export default function Headline() {
   return (
-    <section id="Headline" className="lg:px-20 px-4 py-24">
-      <div className="h-screen md:h-auto flex md:flex-row md:justify-between gap-12 flex-col items-center md:items-start">
+    <section id="Headline" className="lg:px-20 px-4 py-32"> 
+      <div className="flex flex-col md:flex-row items-center gap-12 md:justify-between text-center md:items-start md:text-start">
         <div className="order-2 md:order-1">
           <h1 className="text-primary-900 text-4xl lg:text-5xl xl:text-6xl font-bold">Hi, I'm Cut Risfa Zuhra 👋</h1>
-          <p className="text-primary-600 text-base font-normal mt-2 xl:w-192 w-85 sm:w-142 md:w-125">
+          <p className="text-primary-600 text-base font-normal mt-2 xl:w-192 sm:w-142 md:w-125">
             I'm a beginner front-end developer, working primarily with React.js. My goal is to create great digital experiences that are fast,
             accessible, visually appealing, and responsive. Although I'm new to the field,
             I'm passionate about web development and eager to learn more every day 🦋
           </p>
-          <p className="flex items-center gap-2 text-primary-600 text-base font-normal mt-12">
+          <p className="flex items-center justify-center md:justify-start gap-2 text-primary-600 text-base font-normal mt-12">
             <GoLocation />
             Malang, Indonesia
           </p>
-          <p className="flex items-center gap-3 ml-1 text-primary-600 text-base font-normal mt-2">
+          <p className="flex items-center justify-center md:justify-start gap-3 ml-1 text-primary-600 text-base font-normal mt-2">
             <span class="relative flex h-2 w-2">
               <span class="animate-ping absolute h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
               <span class="relative h-2 w-2 rounded-full bg-emerald-500"></span>
             </span>
             Available for new projects
           </p>
-          <ul className="flex gap-2 mt-12">
+          <ul className="flex gap-2 mt-12 justify-center md:justify-start">
             <li>
               <a href="https://github.com/faracutrisfa" target="blank"><LuGithub size={36} /></a>
             </li>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export default function Navbar() {
 
 
   return (
-    <nav className={`w-full flex justify-between py-6 px-4 md:px-20 fixed top-0 z-50 transition-colors duration-300 ${isScrolled ? "bg-opacity-90 bg-primary-50 text-primary-900 shadow-lg" : "bg-transparent text-primary-600"}`}>
+    <nav className={`w-full flex justify-between py-6 px-4 lg:px-20 fixed top-0 z-50 transition-colors duration-300 ${isScrolled ? "bg-opacity-90 bg-primary-50 text-primary-900 shadow-lg" : "bg-transparent text-primary-600"}`}>
       <img src={Logo} alt="logo" />
       <ul className="gap-6 hidden md:flex">
         <Link
@@ -82,7 +82,7 @@ export default function Navbar() {
         <div
           className={`fixed top-0 right-0 h-full w-2/3 bg-white shadow-lg transition-transform ${click ? 'translate-x-0 duration-500' : 'translate-x-full duration-500'}`}>
           <div className="flex justify-between items-center p-4">
-            <img src={LogoDua} alt="logo" className="h-12" />
+            <img src={LogoDua} alt="logo" className="h-11" />
             <button onClick={handleClick}>
               <IoClose size={24} />
             </button>
