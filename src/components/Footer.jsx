3e0aa4e59593637963d0f21557b99eb46803a317
faracutrisfa <faracutrisfa@gmail.com>
2024-06-09 14:54:44 +0700

    style: add style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <div className="bg-primary-50 flex flex-col items-center text-sm">
+      <div className="bg-primary-50 flex flex-col items-center text-sm py-7">
         <a href="https://www.figma.com/@shahsagarm" target="_blank">© 2023 | Designed ❤️️ by Sagar Shah</a>
         <a href="https://github.com/faracutrisfa/Personal-Portofolio-Cut.git" target="_blank">© 2024 | Code ❤️️ by Cut Risfa Zuhra</a>
       </div>
