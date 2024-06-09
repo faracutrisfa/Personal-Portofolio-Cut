47fe403a6960807c3ec971e934d03783ba27f0e8
faracutrisfa <faracutrisfa@gmail.com>
2024-06-09 15:12:00 +0700

    feat: add fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import LogoDua from "/Logo.svg";
 
 export default function Navbar() {
   const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click)
+  const handleClick = () => setClick(!click);
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -26,13 +26,13 @@ export default function Navbar() {
     };
   }, []);
 
-  const handleScroll = (id) => {
-    const section = document.getElementById(id);
+  const handleLinkClick = (to) => {
+    setClick(false);
+    const section = document.getElementById(to);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
     }
   };
-
 
   return (
     <nav className={`w-full flex justify-between py-6 px-4 lg:px-20 fixed top-0 z-50 transition-colors duration-300 ${isScrolled ? "bg-opacity-90 bg-primary-50 text-primary-900 shadow-lg" : "bg-transparent text-primary-600"}`}>
@@ -41,25 +41,33 @@ export default function Navbar() {
         <Link
           spy={true}
           smooth={true}
-          to="about">
+          to="AboutMe"
+          onClick={() => handleLinkClick("AboutMe")}
+        >
           <li className="hover:border-secondary-900 hover:text-secondary-900">About</li>
         </Link>
         <Link
           spy={true}
           smooth={true}
-          to="work">
+          to="Work"
+          onClick={() => handleLinkClick("Work")}
+        >
           <li className="hover:border-secondary-900 hover:text-secondary-900">Work</li>
         </Link>
         <Link
           spy={true}
           smooth={true}
-          to="testimonials">
+          to="Testimonials"
+          onClick={() => handleLinkClick("Testimonials")}
+        >
           <li className="hover:border-secondary-900 hover:text-secondary-900">Testimonials</li>
         </Link>
         <Link
           spy={true}
           smooth={true}
-          to="contact">
+          to="Contact"
+          onClick={() => handleLinkClick("Contact")}
+        >
           <li className="hover:border-secondary-900 hover:text-secondary-900">Contact</li>
         </Link>
         <li className="ml-6 font-bold text-primary-900 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
@@ -73,14 +81,12 @@ export default function Navbar() {
       </ul>
 
       <div className="block md:hidden relative">
-        <button
-          onClick={handleClick}>
+        <button onClick={handleClick}>
           {click ? <IoClose size={24} /> : <RxHamburgerMenu size={24} />}
         </button>
         {click && <div className="fixed inset-0 bg-black opacity-50"></div>}
 
-        <div
-          className={`fixed top-0 right-0 h-full w-2/3 bg-white shadow-lg transition-transform ${click ? 'translate-x-0 duration-500' : 'translate-x-full duration-500'}`}>
+        <div className={`fixed top-0 right-0 h-full w-2/3 bg-white shadow-lg transition-transform ${click ? 'translate-x-0 duration-500' : 'translate-x-full duration-500'}`}>
           <div className="flex justify-between items-center p-4">
             <img src={LogoDua} alt="logo" className="h-11" />
             <button onClick={handleClick}>
@@ -88,29 +94,37 @@ export default function Navbar() {
             </button>
           </div>
           <hr className="w-full border-t-2 border-primary-50"></hr>
-          <ul className="flex flex-col gap-4 px-4 py-4 font-medium test-base items-center">
+          <ul className="flex flex-col gap-4 px-4 py-4 font-medium text-base items-center">
             <Link
               spy={true}
               smooth={true}
-              to="about">
+              to="AboutMe"
+              onClick={() => handleLinkClick("AboutMe")}
+            >
               <li className="hover:border-secondary-900 hover:text-secondary-900">About</li>
             </Link>
             <Link
               spy={true}
               smooth={true}
-              to="work">
+              to="Work"
+              onClick={() => handleLinkClick("Work")}
+            >
               <li className="hover:border-secondary-900 hover:text-secondary-900">Work</li>
             </Link>
             <Link
               spy={true}
               smooth={true}
-              to="testimonials">
+              to="Testimonials"
+              onClick={() => handleLinkClick("Testimonials")}
+            >
               <li className="hover:border-secondary-900 hover:text-secondary-900">Testimonials</li>
             </Link>
             <Link
               spy={true}
               smooth={true}
-              to="contact">
+              to="Contact"
+              onClick={() => handleLinkClick("Contact")}
+            >
               <li className="hover:border-secondary-900 hover:text-secondary-900">Contact</li>
             </Link>
             <hr className="w-full border-t-2 border-primary-50"></hr>
