38449181cb7d147ffdde7a0448aaf7917e3cf82e
faracutrisfa <faracutrisfa@gmail.com>
2024-06-10 11:10:54 +0700

    feat: add testimonials

--- src/assets/data/Testimoni.js
+++ src/assets/data/Testimoni.js
@@ -1,5 +1,11 @@
+import user from "../icon/user.svg"
+
 export const testimoni = [
     {
-        
-    }
+        profile : user,
+        content : "'She is really dilligent and hard work, i know she Will get what she wants soon'",
+        name : "M. Arkan Anzuye",
+        work : "--",
+        link : "https://www.instagram.com/arkananzuye?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw="
+    },
 ]
--- src/components/LandingPage/Testimonials.jsx
+++ src/components/LandingPage/Testimonials.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { testimoni } from "../../assets/data/Testimoni";
 
 export default function Testimonials() {
     return (
@@ -12,6 +13,18 @@ export default function Testimonials() {
                     Testimonials
                 </motion.button>
                 <h1 className="text-primary-600 font-normal text-lg lg:text-xl">Nice things people have said about me</h1>
+            </div>
+            <div className="gap-12 flex mt-6 md:mt-12 justify-center">
+                {testimoni.map((d, index) => (
+                    <a href={d.link} target="blank" className="items-center justify-center flex flex-col gap-6 border-b rounded-xl bg-white shadow px-12 py-12">
+                        <img src={d.profile} alt="icon" className="border rounded-full px-2 py-2 bg-pink-50 border-pink-50" />
+                        <p className="text-center text-primary-600 text-base font-normal">{d.content}</p>
+                        <div>
+                            <h1 className="text-primary-900 text-lg md:text-xl font-semibold">{d.name}</h1>
+                            <h2 className="text-center text-primary-600 text-sm font-normal">{d.work}</h2>
+                        </div>
+                    </a>
+                ))}
             </div>
         </section>
     )
